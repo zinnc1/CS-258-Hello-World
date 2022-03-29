aa5b077cdc7f902bb648c3a6c932fa1e761448f5
Caleb <zinnc1@sou.edu>
2022-03-29 10:33:16 -0700

    header

---
+++ HelloWorldProgram.h
@@ -0,0 +1,17 @@
+#include <string>
+
+using namespace std;
+
+#ifndef HELLO_WORLD_PROGRAM_
+#define HELLO_WORLD_PROGRAM_
+
+class HelloWorldProgram {
+
+private:
+    string text;
+
+public:
+    HelloWorldProgram();
+
+    void display();
+};
